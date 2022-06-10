6bc9c83baf5582f62112b2ec9d0f334cea00f759
Wrenchy <36445140+teerameth@users.noreply.github.com>
2022-06-11 01:02:15 +0700

    CartPole actor_critic training loop

--- omni/isaac/fiborobotlab/mooncake/examples/simple_CartPole.py
+++ omni/isaac/fiborobotlab/mooncake/examples/simple_CartPole.py
@@ -1,7 +1,10 @@
+import time
+
 import keras
 import tensorflow as tf
+import tensorflow.keras as keras
 import tensorflow.keras.layers as layers
-from tensorflow.python.framework import ops
+import tensorflow.keras.initializers as initializers
 import numpy as np
 import gym
 import matplotlib.pyplot as plt
@@ -17,113 +20,95 @@ def discount_rewards(r):
         running_add = running_add * gamma + r[t]
         discounted_r[t] = running_add
     return discounted_r
+"""Weighted Gaussian log likelihood loss function"""
+def CustomLossGaussian(state, action, reward):
+    # Obtain mu and sigma from actor network
+    nn_mu, nn_sigma = actor_network(state.reshape(-1, 4))
+    # Obtain pdf of Gaussian distribution
+    pdf_value = tf.exp(-0.5 * ((action - nn_mu) / (nn_sigma)) ** 2) * 1 / (nn_sigma * tf.sqrt(2 * np.pi))
+    # Compute log probability
+    log_probability = tf.math.log(pdf_value + 1e-5)
+    # Compute weighted loss
+    loss_actor = - reward * log_probability
+    return loss_actor
 
 class SimpleAgent(keras.Model):
     def __init__(self, s_size, a_size, h_size):
         super(SimpleAgent, self).__init__()
         # Input() can create a placeholder from an arbitrary tf.TypeSpec
         # self.state_in = keras.Input(type_spec=tf.RaggedTensorSpec(shape=[None, s_size], dtype=tf.float32))
-        # self.state_in = keras.Input(shape=[None, s_size], dtype=tf.float32)
-        self.hidden1 = layers.Dense(h_size)
-        self.hidden2 = layers.Dense(a_size, activation='softmax')
+        self.state_in = keras.Input(shape=[None, s_size], dtype=tf.float32)
+        self.hidden1 = layers.Dense(h_size, activation="relu")
+        self.hidden2 = layers.Dense(h_size, activation="relu")
 
-        self.chosen_action = None
-        # The next six lines establish the training procedure. We feed the reward and chosen action into the network
-        # to compute the loss, and use it to update the network.
-        self.reward_holder = keras.Input(shape=[None], dtype=tf.float32)
-        self.action_holder = keras.Input(shape=[None], dtype=tf.int32)
+        self.mu = layers.Dense(a_size, activation="linear", kernel_initializer=initializers.Zeros())# , bias_initializer=initializers.Constant(bias_mu)
+        self.sigma = layers.Dense(a_size, activation="softplus", kernel_initializer=initializers.Zeros())# , bias_initializer=initializers.Constant(bias_sigma)
 
-        # self.indexes = tf.range(0, tf.shape(self.hidden2)[0]) * tf.shape(self.hidden2)[1] + self.action_holder
-        # self.responsible_outputs = tf.gather(tf.reshape(self.hidden2, [-1]), self.indexes)
-        # self.loss = -tf.reduce_mean(tf.math.log(self.responsible_outputs) * self.reward_holder)
-        # tvars = self.trainable_variables()
-        # self.gradient_holders = []
-        # for idx,var in enumerate(tvars):
-        #     placeholder = keras.Input(tf.float32,name=str(idx)+'_holder')
-        #     self.gradient_holders.append(placeholder)
-        # self.gradients = tf.gradients(self.loss, tvars)
-        # optimizer = tf.keras.optimizers.Adam(lr=lr)
-        # self.update_batch = optimizer.apply_gradients(zip(self.gradient_holders, tvars))
-    def call(self, x, training=False, mask=None):
-        # x = self.hidden1(x, training=training)
-        # x = self.hidden2(x, training=training)
-        #
-        # self.chosen_action = tf.argmax(x, 1)  # use maximum value as selected action
-        #
-        # self.indexes = tf.range(0, x.shape[0]) * x.shape[1] + self.action_holder  # 1 from output.shape[0], 4 from output.shape[1]
-        # self.responsible_outputs = tf.gather(tf.reshape(x, [-1]), self.indexes)
-        #
-        #
-        # self.gradient_holders = []
-        # for idx, var in enumerate(self.trainable_variables):
-        #     placeholder = keras.Input(shape=[None], name=str(idx) + '_holder', dtype=tf.float32)
-        #     self.gradient_holders.append(placeholder)
-        with tf.GradientTape() as tape:
-            x = self.hidden1(x, training=training)
-            x = self.hidden2(x, training=training)
-            self.chosen_action = tf.argmax(x, 1)  # use maximum value as selected action
-            self.indexes = tf.range(0, x.shape[0]) * x.shape[1] + self.action_holder  # 1 from output.shape[0], 4 from output.shape[1]
-            self.responsible_outputs = tf.gather(tf.reshape(x, [-1]), self.indexes)
-            self.gradient_holders = []
+    def call(self, inputs, training=False, mask=None):
+        x = self.state_in = inputs
+        x = self.hidden1(x, training=training)
+        x = self.hidden2(x, training=training)
+        return [self.mu(x, training=training), self.sigma(x, training=training)]
 
-            for idx, var in enumerate(self.trainable_variables):
-                placeholder = keras.Input(shape=[None], name=str(idx) + '_holder', dtype=tf.float32)
-                self.gradient_holders.append(placeholder)
-            # loss = -tf.reduce_mean(tf.math.log(self.responsible_outputs) * self.reward_holder)
-            # self.loss = -tf.reduce_mean(tf.math.log(self.responsible_outputs) * self.reward_holder)
-        # self.gradients = tape.gradient(loss, self.trainable_variables)
-        # self.gradients = tf.gradients(self.loss, self.trainable_variables)
-        return x
+actor_network = SimpleAgent(s_size=4, a_size=1, h_size=8)
+print(actor_network.trainable_variables)
+max_angle = 0.418
+opt = keras.optimizers.Adam(learning_rate=0.001)
 
-ops.reset_default_graph() #Clear the Tensorflow graph.
-
-model = SimpleAgent(s_size=4,a_size=2,h_size=8)
-optimizer = tf.keras.optimizers.Adam(lr=1e-2)
-# myAgent = agent(lr=1e-2,s_size=4,a_size=2,h_size=8) #Load the agent.
-
-total_episodes = 5000 #Set total number of episodes to train agent on.
-max_ep = 999
 update_frequency = 5
-
 i = 0
 total_reward = []
 total_length = []
-gradBuffer = model.trainable_variables
-for ix, grad in enumerate(gradBuffer): gradBuffer[ix] = grad * 0
+total_episodes = 10000
+max_episode = 9999
+
+gradBuffer = None
+# gradBuffer = actor_network.trainable_variables
+# for ix,grad in enumerate(gradBuffer): gradBuffer[ix] = grad * 0
 while i < total_episodes:
-    s = env.reset()
+    state = env.reset()
     running_reward = 0
     ep_history = []
-    for j in range(max_ep):
-        #Probabilistically pick an action given our network outputs.
-        print(s)
-        a_dist = model(s.reshape(-1, 4)).numpy()
-        print(a_dist)
-        # a_dist /= a_dist.numpy().sum() # avoid "ValueError: probabilities do not sum to 1"
-        a = np.random.choice(a_dist[0], p=a_dist[0])
-        a = np.argmax(a_dist == a)
-        s1, r, d, _ = env.step(a)  # Get our reward for taking an action given a bandit.
-        ep_history.append([s, a, r, s1])
-        s = s1
-        running_reward += r
-        if d == True:
-            # Update the network.
+    for j in range(max_episode):
+        mu, sigma = actor_network(state.reshape(-1, 4))         # Obtain mu and sigma from network
+        action = tf.random.normal([1], mean=mu, stddev=sigma)   # Draw action from normal distribution
+        # | Num | Action                 |
+        # |-----|------------------------|
+        # | 0   | Push cart to the left  |
+        # | 1   | Push cart to the right |
+        cart_action = 1 if action.numpy().reshape(-1) > 0 else 0 # threshold action since cart_action is discrete
+        next_state, reward, d, _ = env.step(cart_action)
+        # if i % 100 == 0 and i!=0: env.render()
+        delta_angle =  abs(state[2])    # manually calculate reward from falling angle
+        reward = 1 - (delta_angle / max_angle)
+        # env.render()
+        # print(reward, d)
+        # time.sleep(0.1)
+        ep_history.append([state, action, reward, next_state])
+        running_reward += reward
+        if d==True:
+            # Update the network
             ep_history = np.array(ep_history)
             ep_history[:, 2] = discount_rewards(ep_history[:, 2])
-            # feed_dict = {model.reward_holder: ep_history[: 2],
-            #              model.action_holder: ep_history[:, 1],
-            #              model.state_in: np.vstack(ep_history[:, 0])
-            #              }
-            # grads = model.gradients(feed_dict, model.trainable_variables)
-            # for idx, grad in enumerate(grads): gradBuffer[idx] += grad
-            if i % update_frequency == 0 and i != 0:
-                # feed_dict = dictionary = dict(zip(model.gradient_holders, gradBuffer))
-                optimizer.apply_gradients(zip(model.gradient_holders, model.trainable_variables))
-                # model.update_batch()    # feed_dict= dictionary = dict(zip(myAgent.gradient_holders, gradBuffer))
+            if gradBuffer is None:  # init gradBuffer
+                gradBuffer = actor_network.trainable_variables
                 for ix, grad in enumerate(gradBuffer): gradBuffer[ix] = grad * 0
+            with tf.GradientTape() as tape:
+                # Compute Gaussian loss
+                loss_value = CustomLossGaussian(state, action, reward)
+                # Compute gradients
+                grads = tape.gradient(loss_value, actor_network.trainable_variables)
+                # Apply gradients to update network weights
+                # opt.apply_gradients(zip(grads, actor_network.trainable_variables))
+
+            for idx, grad in enumerate(grads): gradBuffer[idx] += grad
+            if i % update_frequency == 0 and i != 0:
+                # Apply gradients to update network weights
+                opt.apply_gradients(zip(gradBuffer, actor_network.trainable_variables))
+                for ix, grad in enumerate(gradBuffer): gradBuffer[ix] = grad * 0    # reset buffer
             total_reward.append(running_reward)
             total_length.append(j)
             break
-    if i % 100 == 0:
-        print(np.mean(total_reward[-100:]))
+        state = next_state
+    if i % 100 == 0: print(np.mean(total_reward[-100:]))
     i += 1
