64ccaf041bd8f73d5da75422e8ec61f5a6b187ca
BotTherapist <robotfriend.research@gmail.com>
2022-06-15 03:10:57 +0700

    fix state save

--- omni/isaac/fiborobotlab/obike/train_obike_td3.py
+++ omni/isaac/fiborobotlab/obike/train_obike_td3.py
@@ -20,13 +20,13 @@ from omni.isaac.imu_sensor import _imu_sensor
 
 print(tf.config.list_physical_devices('GPU'))
 
-state_low = [-10, -10, -10]
-state_high = [10, 10, 10]
+state_low = [-100, -100, -10]
+state_high = [100, 100, 10]
 action_low = [-1]
 action_high = [1]
 
 ## Specify simulation parameters ##
-_physics_dt = 1/1000
+_physics_dt = 1/100
 _rendering_dt = 1/30
 _max_episode_length = 60/_physics_dt # 60 second after reset
 _iteration_count = 0
@@ -60,7 +60,7 @@ _sensor_handle = imu_interface.add_sensor_on_body("/obike/chassic", props)
 
 from td3 import RBuffer, Critic, Actor, Agent
 with tf.device('GPU:0'):
-    tf.random.set_seed(336699)
+    # tf.random.set_seed(336699)
     agent = Agent(n_action=1, n_state=3, action_low=action_low, action_high=action_high)
     episods = 20000
     ep_reward = []
@@ -71,48 +71,67 @@ with tf.device('GPU:0'):
         if target == True:
             break
         total_reward = 0
-        state = world.reset()
+        world.reset()
         done = False
-        previous = {'robot_position': None, 'robot_rotation': None, 'fall_rotation': None}
-        present = {'robot_position': None, 'robot_rotation': None, 'fall_rotation': None}
+        previous = {'robot_position': None, 'robot_rotation': None, 'fall_rotation': None, "lin_acc_y": None, "lin_acc_z": None, "ang_vel_x": None}
+        present = {'robot_position': None, 'robot_rotation': None, 'fall_rotation': None, "lin_acc_y": None, "lin_acc_z": None, "ang_vel_x": None}
         while not done:
-            previous['robot_position'], previous['robot_rotation'] = robot.get_world_pose()
-            previous['fall_rotation'] = q2falling(previous['robot_rotation'])
-            reading = imu_interface.get_sensor_readings(_sensor_handle)
-            if reading.shape[0] == 0:  # no valid data in buffer -> init observation wih zeros
-                observations = np.array([0, 0, 0])
-            else:  # IMU will  return [???, acc_x, acc_y, acc_z, gyr_x, gyr_y, gyr_z]
-                observations = np.array([reading[-1]["lin_acc_y"],  # Use only lastest data in buffer
-                                         reading[-1]["lin_acc_z"],
-                                         reading[-1]["ang_vel_x"]])
+            if previous['robot_position'] is None:  # initial state
+                robot_position, _ = robot.get_world_pose()
+                previous = {'robot_position': robot_position[0],
+                            'robot_rotation': robot_position[1],
+                            'fall_rotation': robot_position[2],
+                            "lin_acc_y": 0,
+                            "lin_acc_z": 0,
+                            "ang_vel_x": 0}
+            state = np.array([previous["lin_acc_y"],
+                              previous["lin_acc_z"],
+                              previous["ang_vel_x"]], dtype=np.float32)
+
             ## Scale accel from (-1000, 1000) -> (0, 1) for NN inputs
             ## Scale gyro from (-4.36, 4.36) -> (0, 1) for NN inputs (4.36 rad = 250 deg)
             # print(observations)
             # time.sleep(1)
-            observations[0] = (observations[0] / 2000) + 0.5
-            observations[1] = (observations[1] / 2000) + 0.5
-            observations[2] = (observations[2] / 8.72) + 0.5
-            # observations = np.array(observations, dtype=np.float32).reshape((batch_size, num_timesteps, input_dim))  # add extra dimension for batch_size=1
-            observations = np.array(observations, dtype=np.float32)
+            # observations[0] = (state[0] / 2000) + 0.5
+            # observations[1] = (state[1] / 2000) + 0.5
+            # observations[2] = (state[2] / 8.72) + 0.5
 
-            action = agent.act(observations)
-
+            action = agent.act(state)
+            # print(action)
             ## EXECUTE ACTION ##
             from omni.isaac.core.utils.types import ArticulationAction
             robot.apply_wheel_actions(ArticulationAction(joint_efforts=[action * 100 * 0.607, 0, 0]))
             world.step(render=True)
+            reading = imu_interface.get_sensor_readings(_sensor_handle)
+            if reading.shape[0] == 0:  # no valid data in buffer -> init observation wih zeros
+                observations = np.array([0, 0, 0], dtype=np.float32)
+                print("Warning! no data in IMU buffer")
+            else:  # IMU will  return [???, acc_x, acc_y, acc_z, gyr_x, gyr_y, gyr_z]
+                present["lin_acc_y"], present["lin_acc_z"], present["ang_vel_x"] = reading[-1]["lin_acc_y"], reading[-1]["lin_acc_z"], reading[-1]["ang_vel_x"]
+                next_state = np.array([present["lin_acc_y"],
+                                       present["lin_acc_z"],
+                                       present["ang_vel_x"]])
+
             present['robot_position'], present['robot_rotation'] = robot.get_world_pose()
             present['fall_rotation'] = q2falling(present['robot_rotation'])
-            reward = previous['fall_rotation'] - present['fall_rotation']  # calculate reward from movement toward center
+            reward = 1
+            # reward = previous['fall_rotation'] - present['fall_rotation']  # calculate reward from movement toward center
+
             ## Check for stop event ##
             exceed_time_limit = world.current_time_step_index >= _max_episode_length
-            robot_fall = True if previous['fall_rotation'] > 50 / 180 * math.pi else False
+            robot_fall = True if present['fall_rotation'] > 50 / 180 * math.pi else False
             done = exceed_time_limit or robot_fall
+
+            agent.savexp(state, next_state, action, done, reward)
+            agent.train()
+
+            ## UPDATE STATE ##
+            previous = present.copy()
             total_reward += reward
+
             if done:
                 ep_reward.append(total_reward)
                 avg_reward = np.mean(ep_reward[-100:])
                 total_avgr.append(avg_reward)
                 print("total reward after {} steps is {} and avg reward is {}".format(s, total_reward, avg_reward))
-                if avg_reward == 200:
-                    target = True
+                if avg_reward == 200: target = True
