40a65d259b48cbd135e90c2d5ae8948136677052
Wrenchy <36445140+teerameth@users.noreply.github.com>
2022-06-18 11:01:40 +0700

    migrate to IsaacSim 2022.1.0

---
+++ omni/isaac/fiborobotlab/cartpole/rlgames_train.py
@@ -0,0 +1,98 @@
+from omniisaacgymenvs.utils.hydra_cfg.hydra_utils import *
+from omniisaacgymenvs.utils.hydra_cfg.reformat import omegaconf_to_dict, print_dict
+from omniisaacgymenvs.utils.rlgames.rlgames_utils import RLGPUAlgoObserver, RLGPUEnv
+from omniisaacgymenvs.utils.config_utils.path_utils import retrieve_checkpoint_path
+from omniisaacgymenvs.envs.vec_env_rlgames import VecEnvRLGames
+
+import hydra
+from omegaconf import DictConfig
+
+from rl_games.common import env_configurations, vecenv
+from rl_games.torch_runner import Runner
+
+import os
+
+class RLGTrainer():
+    def __init__(self, cfg, cfg_dict):
+        self.cfg = cfg
+        self.cfg_dict = cfg_dict
+
+    def launch_rlg_hydra(self, env):
+        # `create_rlgpu_env` is environment construction function which is passed to RL Games and called internally.
+        # We use the helper function here to specify the environment config.
+        self.cfg_dict["task"]["test"] = self.cfg.test
+
+        # register the rl-games adapter to use inside the runner
+        vecenv.register('RLGPU',
+                        lambda config_name, num_actors, **kwargs: RLGPUEnv(config_name, num_actors, **kwargs))
+        env_configurations.register('rlgpu', {
+            'vecenv_type': 'RLGPU',
+            'env_creator': lambda **kwargs: env
+        })
+
+        self.rlg_config_dict = omegaconf_to_dict(self.cfg.train)
+
+    def run(self):
+        # create runner and set the settings
+        runner = Runner(RLGPUAlgoObserver())
+        runner.load(self.rlg_config_dict)
+        runner.reset()
+
+        # dump config dict
+        experiment_dir = os.path.join('runs', self.cfg.train.params.config.name)
+        os.makedirs(experiment_dir, exist_ok=True)
+        with open(os.path.join(experiment_dir, 'config.yaml'), 'w') as f:
+            f.write(OmegaConf.to_yaml(self.cfg))
+
+        runner.run({
+            'train': not self.cfg.test,
+            'play': self.cfg.test,
+            'checkpoint': self.cfg.checkpoint,
+            'sigma': None
+        })
+
+
+@hydra.main(config_name="config", config_path="./cfg")
+def parse_hydra_configs(cfg: DictConfig):
+
+    # ensure checkpoints can be specified as relative paths
+    if cfg.checkpoint:
+        cfg.checkpoint = retrieve_checkpoint_path(cfg.checkpoint)
+        if cfg.checkpoint is None:
+            quit()
+
+    cfg_dict = omegaconf_to_dict(cfg)
+    print_dict(cfg_dict)
+
+    headless = cfg.headless
+
+    env = VecEnvRLGames(headless=headless)
+
+    from scripts.sim_config import SimConfig
+    sim_config = SimConfig(cfg_dict)
+
+    cfg = DictConfig(sim_config.config)
+
+    from cartpole_task import CartpoleTask
+    task = CartpoleTask(name="Cartpole",
+                        sim_config=sim_config,
+                        env=env
+    )
+
+    env.set_task(task=task, sim_params=sim_config.get_physics_params(), backend="torch", init_sim=True)
+
+    # task = initialize_task(cfg_dict, env)
+    print(cfg)
+    # sets seed. if seed is -1 will pick a random one
+    from omni.isaac.core.utils.torch.maths import set_seed
+    cfg.seed = set_seed(cfg.seed, torch_deterministic=cfg.torch_deterministic)
+
+    rlg_trainer = RLGTrainer(cfg, cfg_dict)
+    rlg_trainer.launch_rlg_hydra(env)
+    rlg_trainer.run()
+
+    env.close()
+
+
+if __name__ == '__main__':
+    parse_hydra_configs()
